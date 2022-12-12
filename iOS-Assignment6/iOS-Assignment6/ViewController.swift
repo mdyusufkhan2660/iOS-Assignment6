f46590893a897ac10dc5848c4dbcf91aeeb71e7a
Md. Yusuf Khan <md.yusuf.khan.2660@gmail.com>
2022-12-12 11:08:07 -0500

    Added double tap navigation

--- iOS-Assignment6/iOS-Assignment6/ViewController.swift
+++ iOS-Assignment6/iOS-Assignment6/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func didTapView(_ sender: UITapGestureRecognizer) {
+        
+        if sender.numberOfTapsRequired == 1 {
+            print("Single Tap")
+        }
+        else if(sender.numberOfTapsRequired == 2){
+            print("Double Tap")
+            self.performSegue(withIdentifier: "showSecond", sender: self)
+        }
+        
+    }
+    
+    @IBOutlet var myView: UIView!{
+        didSet{
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapView(_:)))
+        
+        // Configure Tap Gesture Recognizer
+        tapGestureRecognizer.numberOfTapsRequired = 2
+
+           // Add Tap Gesture Recognizer
+        myView.addGestureRecognizer(tapGestureRecognizer)
+
     }
-
-
 }
 
